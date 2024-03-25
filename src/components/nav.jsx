a88eb0f56d7460bab4f523b48228508d28a37cc2
lloydbutay123 <johnlloydbutay123@gmail.com>
2024-03-25 14:51:41 +0800

    added the quotation section

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -57,8 +57,6 @@ const nav = () => {
             </li>
           </Link>
         </ul>
-        <Footer />
-
       </div>
     </>
   );
--- src/components/quotes.jsx
+++ src/components/quotes.jsx
@@ -1,12 +1,41 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useState } from "react";
+import bgimg from "../assets/background_images/vector.png";
 
 const quotes = () => {
-    const [quote, setQuote] = useState("");
+  let randomQuotes = [];
+  const delay = 2 * 60 * 1000;
+
+  async function loadQuotes() {
+    const res = await fetch("https://type.fit/api/quotes");
+    randomQuotes = await res.json();
+  }
+  const [quote, setQuote] = useState({
+    text: "Choose a job you love and you will never have to work a day in your life",
+    author: "confucius",
+  });
+
+  const random = () => {
+    const select =
+      randomQuotes[Math.floor(Math.random() * randomQuotes.length)];
+    setQuote(select);
+  };
+
+  setTimeout(random, delay);
+  loadQuotes();
 
   return (
-    <section className="quote" id="quote">
-      <h1 className="text-[20px]">{quote}</h1>
+    <section
+      className="relative quote min-h-[100vh] px-[20px] w-full flex justify-center items-center"
+      id="quote"
+    >
+      <div className="md:w-2/3">
+        <div className="text-center">
+          <h1 className="text-[5vw] md:text-[40px] font-bold">" {quote.text} "</h1>
+          <p className="text-end text-gray-500">{quote.author}</p>
+        </div>
+      </div>
+      <img src={bgimg} className="absolute w-[60vw] md:w-[30%] opacity-30"/>
     </section>
   );
 };
